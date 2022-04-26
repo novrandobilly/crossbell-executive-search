491bfa00769b49f5ee937ccbb8823364eeb0e1b4
novrandobilly <novrandobilly@gmail.com>
2022-04-26 19:57:29 +0700

    add client close db

--- pages/api/talent-registration/register.ts
+++ pages/api/talent-registration/register.ts
@@ -4,14 +4,16 @@ import { connectToDatabase } from '../../../lib/db';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
+    let client = await connectToDatabase();
     try {
-      let client = await connectToDatabase();
       let db = client.db('crossbell-executive-search');
       let collection = db.collection('talents');
       let result = await collection.insertOne(req.body);
+      client.close();
       res.status(200).json(result);
     } catch (err) {
       console.log(err);
+      client.close();
       if (err instanceof Error) {
         res.status(500).json({ error: err.message });
       } else {
